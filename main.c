87c28ae6ecbb373188aa56ec05231b87b56a6225
Rox Teddy <RoxTeddy@pc61.home>
2015-01-22 03:07:14 +0100

    ft_atoi tests

--- main.c
+++ main.c
@@ -6,15 +6,24 @@
 /*   By: mfebvay <mfebvay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/20 20:02:40 by mfebvay           #+#    #+#             */
-/*   Updated: 2015/01/20 20:11:00 by mfebvay          ###   ########.fr       */
+/*   Updated: 2015/01/20 21:40:15 by mfebvay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int		main(void)
 {
-	printf("%d\n", ft_atoi("123"));
+	printf("'42' with atoi() : %d\n", atoi("42"));
+	printf("'42' with ft_atoi() : %d\n", ft_atoi("42"));
+	printf("'+42' with atoi() : %d\n", atoi("+42"));
+	printf("'+42' with ft_atoi() : %d\n", ft_atoi("+42"));
+	printf("'-42' with atoi() : %d\n", atoi("-42"));
+	printf("'-42' with ft_atoi() : %d\n", ft_atoi("-42"));
+	printf("'+-42' with atoi() : %d\n", atoi("+-42"));
+	printf("'+-42' with ft_atoi() : %d\n", ft_atoi("+-42"));
+
 	return (0);
 }
